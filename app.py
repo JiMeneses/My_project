67fb9ac0265f808f0c9e28d6bc2bd197f78d0264
JiMeneses92 <jimeneses03081992@gmail.com>
2025-06-27 12:13:22 -0600

    Actualizar

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import plotly.express as px
 
 car_data = pd.read_csv(
     'vehicles_us.csv')  # cargar datos
-hist_button = st.header('Construir histograma')  # crear boton
+# hist_button = st.header('Construir histograma')  # crear boton
+hist_button = st.button('Construir histograma')  # crear boton
 
 if hist_button:
 
