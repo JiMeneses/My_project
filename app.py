23758ac666a13c965f78d56915fc88b0349e36b3
JiMeneses92 <jimeneses03081992@gmail.com>
2025-06-26 21:02:11 -0600

    create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+car_data = pd.read_csv(
+    'D:/Tripleten/Sprint7/My_project/vehicles_us.csv')  # cargar datos
+hist_button = st.header('Construir histograma')  # crear boton
+
+if hist_button:
+
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de ventas de coches')
+
+    px.histogram(car_data, x='odometer')
+    st.ploty_chart(fig, use_container_width=True)
