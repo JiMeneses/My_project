7c810175e3cb9f50fc08c97f10e1ca5e8cb111d9
JiMeneses92 <jimeneses03081992@gmail.com>
2025-06-27 12:43:51 -0600

    Actualizar

--- app.py
+++ app.py
@@ -5,13 +5,25 @@ import plotly.express as px
 
 car_data = pd.read_csv(
     'vehicles_us.csv')  # cargar datos
-# hist_button = st.header('Construir histograma')  # crear boton
-hist_button = st.button('Construir histograma')  # crear boton
 
-if hist_button:
+st.header('Anuncios de ventas de autos')  # crear encabezado
+
+hist_checkbox = st.checkbox('Construir histograma')  # crear checkbox
+scatter_checkbox = st.checkbox(
+    'Construir gráfico de dispersión')  # crear checkbox
+
+if hist_checkbox:
 
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de ventas de coches')
 
     fig = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_checkbox:
+
+    st.write(
+        'Creación de gráfico de dispersión para el conjunto de datos de anuncios de ventas de coches')
+
+    fig = px.scatter(car_data, y='price', x='odometer')
     st.plotly_chart(fig, use_container_width=True)
